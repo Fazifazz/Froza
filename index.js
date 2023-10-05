ba84421ba8be8cbecafe59c754eead3a94da685e
fasil <fazzfasi7@gmail.com>
2023-10-05 18:50:39 +0530

    admin login scripts and css added

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.use('/admin', require('./routes/adminRoutes'))
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => console.log('Server running...')) 
